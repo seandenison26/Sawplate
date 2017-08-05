63dbee28cd7ecfe59edc1ba225ca67bd431e88bd
ziggy <sean.denison26@gmail.com>
2017-08-05 17:21:20 -0500

    reduced lint errors to no unused expressions, no nil

--- server/reducers.js
+++ server/reducers.js
@@ -1,12 +1,11 @@
 export function bigDaddyReduce (state = {serverCalls: 0}, action) {
-				switch(action.type) {
-					case "INCREASE_SERVER_CALLS":
-						return Object.assign({},state, {serverCalls: state.serverCalls + 1})
-				
-					default:
-						return state;
-				}
-};
+	switch(action.type) {
+	case 'INCREASE_SERVER_CALLS':
+		return Object.assign({},state, {serverCalls: state.serverCalls + 1})
+	default:
+		return state
+	}
+}
 
  
  

3bef5acb0d5351834ff74090f290f5df006f60c4
ziggy <sean.denison26@gmail.com>
2017-08-05 12:11:02 -0500

    npm run dev now hosts the server and checks for build changes

--- server/reducers.js
+++ server/reducers.js
@@ -1,4 +1,4 @@
-module.exports.bigDaddyReduce = (state = {serverCalls: 0}, action) => {
+export bigDaddyReduce = (state = {serverCalls: 0}, action) => {
 				switch(action.type) {
 					case "INCREASE_SERVER_CALLS":
 						return Object.assign({},state, {serverCalls: state.serverCalls + 1})

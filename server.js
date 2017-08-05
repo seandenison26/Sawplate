432e46115bd299d24022d9e3cf39ddacf8fcfbb4
ziggy <sean.denison26@gmail.com>
2017-08-05 10:47:02 -0500

    Linter only fails with no require, no-nil, no-unused-expressions

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
-const express = require("express");
-const redux = require("redux");
-const reducers = require("./reducers");
-const cors = require("cors");
+const express = require('express')
+const redux = require('redux')
+const reducers = require('./reducers')
+const cors = require('cors')
 
-const store = redux.createStore(reducers.bigDaddyReduce);
+const store = redux.createStore(reducers.bigDaddyReduce)
 
-store.subscribe(() => {console.log(store.getState())});
+const app = express()
 
-const app = express();
-
-app.use(cors());
+app.use(cors())
 
 app.get('/:action', function (req, res) {
-	store.dispatch({type:req.params.action});
-	console.log(`This is call ${store.getState().serverCalls}!`);
+	store.dispatch({type:req.params.action})
 	
-	
-	res.json(JSON.stringify({type:"SERVER_TOTAL",serverTotal:store.getState().serverCalls}))
+	res.json(JSON.stringify({type:'SERVER_TOTAL',serverTotal:store.getState().serverCalls}))
 })
 
 app.listen(3000, function () {
-	console.log('Example app listening on port 3000!')
 })
 
-const poop = {};
-poop.x = 10;
 
-for(var x;;) {
-
-}
